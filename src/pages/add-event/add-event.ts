e982e8525340f928f4d3a01cec1c17c6bb1ac285
s64309010033 <s64309010033@ptnicec.ac.th>
2022-10-25 17:54:42 +0700

    ส่งครั้งสุดท้าย

--- src/pages/add-event/add-event.ts
+++ src/pages/add-event/add-event.ts
@@ -2,30 +2,47 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams, AlertController } from 'ionic-angular';
 import { Calendar } from '@ionic-native/calendar';
 
+
+
 @IonicPage()
 @Component({
-  selector: 'page-edit-event',
-  templateUrl: 'edit-event.html',
+  selector: 'page-add-event',
+  templateUrl: 'add-event.html',
 })
-export class EditEventPage {
+export class AddEventPage {
 
   event = { title: "", location: "", message: "", startDate: "", endDate: "" };
-  oldEvent = { title: "", location: "", message: "", startDate: "", endDate: "" };
 
   constructor(public alertCtrl: AlertController,
     public navCtrl: NavController,
     public navParams: NavParams,
     private calendar: Calendar) {
-      this.event = navParams.get("event");
-      var startDate = new Date(navParams.get("event").startDate.replace(/\s/, 'T')).toISOString();
-      var endDate = new Date(navParams.get("event").endDate.replace(/\s/, 'T')).toISOString();
-      this.event.startDate = startDate;
-      this.event.endDate = endDate;
-      this.oldEvent = this.event;
   }
 
   ionViewDidLoad() {
-    console.log(this.event);
+    console.log('ionViewDidLoad AddEventPage');
+  }
+
+  save() {
+    this.calendar.createEvent(this.event.title, this.event.location, this.event.message, new Date(this.event.startDate), new Date(this.event.endDate)).then(
+      (msg) => {
+        let alert = this.alertCtrl.create({
+          title: 'Success!',
+          subTitle: 'Event saved successfully',
+          buttons: ['OK']
+        });
+        alert.present();
+        this.navCtrl.pop();
+      },
+      (err) => {
+        let alert = this.alertCtrl.create({
+          title: 'Failed!',
+          subTitle: err,
+          buttons: ['OK']
+        });
+        alert.present();
+      }
+    );
   }
 
 }
